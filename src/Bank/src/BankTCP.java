709068f4fdb3f57b70209250a0a1876872b68175
stdatrann <dangquang.tran@stud.h-da.de>
2023-05-21 21:06:45 +0200

    added more Thread support for UDP and TCP connections

--- src/Bank/src/BankTCP.java
+++ src/Bank/src/BankTCP.java
@@ -1,6 +1,21 @@
 import TCP.src.TCPInterface;
 
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+
 public class BankTCP implements TCPInterface {
+    public ServerSocket getServerSocket() {
+        return serverSocket;
+    }
+
+    private ServerSocket serverSocket;
+
+    public BankTCP(int port) throws IOException {
+        this.serverSocket = new ServerSocket(port);
+        System.out.println("Bank TCP server started on address " + InetAddress.getLocalHost().getHostAddress() + " on port " + port);
+    }
+
     @Override
     public void connect(String host, int port) {
 
