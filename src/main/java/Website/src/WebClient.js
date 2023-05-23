21fca41b5f00bde076f69c9edd6ec6a9d7fb5583
stdatrann <dangquang.tran@stud.h-da.de>
2023-05-23 12:51:27 +0200

    TCP connection enabled

--- src/main/java/Website/src/WebClient.js
+++ src/main/java/Website/src/WebClient.js
@@ -1,41 +1,33 @@
-const WebSocket = require('ws');
 const net = require('net');
 
-// Create a WebSocket server
-const wss = new WebSocket.Server({ port: 8080 });
+// Retrieve the Java server's IP address and port from environment variables
+const bankIp = '172.20.1.1';
+const bankPort = 8000;
 
-// Create a TCP socket server
-const tcpServer = net.createServer();
-
-// Handle WebSocket connection
-wss.on('connection', function connection(ws) {
-    console.log('WebSocket connection established.');
-
-    // Create a TCP socket connection to the Java server
-    const tcpSocket = net.createConnection({ port: 8000 }, function () {
-        console.log('Connected to Java server.');
-
-        // Pass data received from the WebSocket to the TCP server
-        ws.on('message', function incoming(message) {
-            console.log('Received message from WebSocket:', message.toString());
-            tcpSocket.write(message);
-        });
-
-        // Pass data received from the TCP server to the WebSocket
-        tcpSocket.on('data', function (data) {
-            console.log('Received data from Java server:', data.toString());
-            ws.send(data.toString());
-        });
-
-        // Handle the closure of the WebSocket connection
-        ws.on('close', function close() {
-            console.log('WebSocket connection closed.');
-            tcpSocket.end();
-        });
+// Create a TCP socket connection to the Java server
+const tcpSocket = net.createConnection({ host: bankIp, port: bankPort }, function () {
+    console.log('Connected to Java server.');
+    tcpSocket.write('Hello, Java server!');
+    // Handle the closure of the TCP connection
+    tcpSocket.on('close', function () {
+        console.log('TCP connection closed.');
     });
+
+    // Pass data received from the TCP server to the console
+    tcpSocket.on('data', function (data) {
+        console.log('Received data from Java server:', data.toString());
+    });
+
+    // Send data to the TCP server
+    tcpSocket.write('Hello, Java server!');
 });
 
-// Start the TCP server
-tcpServer.listen(8000, function () {
-    console.log('TCP server listening on port 8000');
+// Handle connection errors
+tcpSocket.on('error', function (error) {
+    console.error('Error connecting to Java server:', error);
+});
+
+// Handle the closure of the TCP connection
+tcpSocket.on('close', function () {
+    console.log('TCP connection closed.');
 });
