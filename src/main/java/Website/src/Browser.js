67936e63db28ba42d7fdf176db4976b7a524be88
Dang Quang Tran <dangquang.tran@stud.h-da.de>
2023-05-24 22:33:00 +0000

    bring JS content from Browser.html to Browser.js

--- src/main/java/Website/src/Browser.js
+++ src/main/java/Website/src/Browser.js
@@ -1,95 +1,23 @@
-let socket;
-function establishConnection() {
-    socket = new WebSocket("ws://172.20.1.1:8000");
-
-    socket.onopen = function () {
-        console.log("Connected to Bank server");
-        socket.send("Request from browser");
-    };
-
-    socket.onmessage = function (event) {
-        console.log("Received response: " + event.data);
-        // Process the response received from the bank server
-    };
-
-    socket.onclose = function () {
-        console.log("Connection closed");
-    };
-    console.log("Connected to Bank server");
-    fetch('http://172.20.1.1:8000')
-        .then(response => {
-            // Output the response status code to the console
-            console.log('Response status code:', response.status);
+function getMsg() {
+    // Creates a promise object for retrieving the desired data
+    fetch("http://172.20.2.1:8002/api")
+        // When received, exposes the JSON component
+        .then((response) => {
+            return response.json();
         })
-        .then(response => response.json())
-        .then(data => {
-            // Process the response from the Java application
-            console.log(data);
-        })
-        .catch(error => {
-            // Handle the error
-            console.error(error);
+        // Displays the message on the page
+        .then((json) => {
+            document.getElementById("msg").innerHTML = "Server message: " + json.msg;
         });
 }
-
-function pingURL() {
-
-    // The custom URL entered by user
-    const URL = $("#url").val();
-    const settings = {
-
-        // Defines the configurations
-        // for the request
-        cache: false,
-        dataType: "jsonp",
-        async: true,
-        crossDomain: true,
-        url: URL,
-        method: "GET",
-        headers: {
-            accept: "application/json",
-            "Access-Control-Allow-Origin": "*",
-        },
-
-        // Defines the response to be made
-        // for certain status codes
-        statusCode: {
-            200: function (response) {
-                console.log("Status 200: Page is up!");
-            },
-            400: function (response) {
-                console.log("Status 400: Page is down.");
-            },
-            0: function (response) {
-                console.log("Status 0: Page is down.");
-            },
-        },
-    };
-
-    // Sends the request and observes the response
-    $.ajax(settings).done(function (response) {
-        console.log(response);
+// Sends message to server via POST
+function postMsg() {
+    const data = {msg: document.getElementById("msg-box").value};
+    // Creates a promise object for sending the desired data
+    fetch("http://172.20.2.1:8002/api",{
+        method: "POST",
+        // Format of the body must match the Content-Type
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify(data)
     });
 }
-
-/*function totalValueRequest() {
-    socket.addEventListener('open', function (event) {
-        socket.send("Request total Value of the Bank");
-    });
-
-    socket.addEventListener('message', function (event) {
-        document.getElementById('totalValue').value = event.data;
-    });
-}
-
-function moneyAdd() {
-    socket.addEventListener('open', function (event) {
-        socket.send("+" . document.getElementById('changeMoney'))
-    });
-}
-
-function moneySubtract() {
-    socket.addEventListener('open', function (event) {
-        socket.send("-" . document.getElementById('changeMoney'))
-    });
-}*/
